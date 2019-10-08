6ca38497de6a3f627b9e2bc12aa5821ebb06e871
Michael Lean Pedrasoli <pedrasolimichael@gmail.com>
2019-10-08 07:43:19 -0300

    Insert CORS in application

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import * as restify from 'restify';
 import * as mongoose from 'mongoose';
 import * as fs from 'fs';
+import * as corsMiddleware from 'restify-cors-middleware';
 
 import { environment } from '../common/environment';
 import { Router } from '../common/router';
@@ -40,10 +41,21 @@ export class Server {
 
                 this.application = restify.createServer(options);
 
+                const corsOptions: corsMiddleware.Options = {
+                    preflightMaxAge: 10,
+                    origins: ['*'],
+                    allowHeaders: ['Authorization'],
+                    exposeHeaders: ['x-custom-header']
+                }
+
+                const cors: corsMiddleware.CorsMiddleware = corsMiddleware(corsOptions);
+
+                this.application.pre(cors.preflight);
+
                 this.application.pre(restify.plugins.requestLogger({
                     log: logger
                 }))
-
+                this.application.use(cors.actual);
                 this.application.use(restify.plugins.queryParser());
                 this.application.use(restify.plugins.bodyParser());
                 this.application.use(mergePatchBodyParser);
